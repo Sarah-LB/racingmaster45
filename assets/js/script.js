4b1dac0bf7e09833ec56fe84cc7f76e371e07a82
Sarah-LB <slloving@gmail.com>
2023-06-22 16:15:00 +0000

    change contact form funstionality

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+const contactForm = document.querySelector("#contact-form");
+const submitBtn = document.querySelector(".submit-btn");
+const nameInput = document.querySelector("#user_name");
+const emailInput = document.querySelector("#user_email");
+const messageInput = document.querySelector("#message");
+
+const publicKey = "VLMZigXcO5Upo8sX1";
+const serviceID = "contact_services";
+const templateID = "contact_form";
+
+emailjs.init(publicKey);
+
+contactForm.addEventListener("submit", e => {
+    e.preventDefault();
+    submitBtn.Btn.innerText = "Sending...";
+    const inputFields = {
+        name: nameInput.value,
+        email: emailInput.value,
+        message: messageInput.value
+    }
+    emailjs.send(serviceID, templateID, inputFields)
+    .then(() => {
+        submitBtn,innerText = "Message Sent";
+        nameInput.value="";
+        emailInput.value="";
+        messageInput.value="";
+    }, (error); => {
+    submitBtn.innText = "Something went wrong";
+    });
+});
